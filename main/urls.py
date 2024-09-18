9dd448edae9af1689d9a8ef911de612ebe9f435f
unknown <favourelodimuor16@gmail.com>
2024-09-18 17:24:40 +0100

    first commit

--- main/urls.py
+++ main/urls.py
@@ -4,9 +4,27 @@ from django.urls import path
 urlpatterns = [
     path('', main_views.home_view, name='home'),
     path('about/', main_views.about_view, name='about'),
-    path('services/', main_views.services_view, name='services'),
-    path('financial-info/', main_views.financial_info_view, name='financial_info'),
-    path('ministries/', main_views.ministries_view, name='ministries'),
-    path('news-events/', main_views.news_events_view, name='news_events'),
-    path('give-a-gift/', main_views.give_a_gift_view, name='give_a_gift'),
+
+    path('services/for-individuals/', main_views.services_view, name='services_individuals'),
+    path('services/for-churches-institutions/', main_views.services_view, name='services_churches_institutions'),
+    path('services/for-professional-advisory/', main_views.services_view, name='services_professional_advisory'),
+
+    path('ministries/grants/', main_views.ministries_view, name='ministries_grants'),
+    path('ministries/seminary-scholarships/', main_views.ministries_view, name='ministries_seminary_scholarships'),
+    path('ministries/clergy-leadership-training/', main_views.ministries_view,
+         name='ministries_clergy_leadership_training'),
+
+    path('financial-info/terms-to-know/', main_views.financial_info_view, name='financial_info_terms_to_know'),
+    path('financial-info/investment-philosophy/', main_views.financial_info_view,
+         name='financial_info_investment_philosophy'),
+    path('financial-info/wespath-institutional-investments/', main_views.financial_info_view,
+         name='financial_info_wespath_institutional_investments'),
+
+    path('news-events/foundation-news/', main_views.news_events_view, name='news_events_foundation_news'),
+    path('news-events/legacies-newsletter/', main_views.news_events_view, name='news_events_legacies_newsletter'),
+    path('news-events/market-financial-news/', main_views.news_events_view, name='news_events_market_financial_news'),
+    path('news-events/events/', main_views.news_events_view, name='news_events_events'),
+
+    path('give-a-gift/donate-now/', main_views.give_a_gift_view, name='give_a_gift_donate_now'),
+    path('give-a-gift/ways-to-give/', main_views.give_a_gift_view, name='give_a_gift_ways_to_give'),
 ]
--- main/views.py
+++ main/views.py
@@ -14,20 +14,61 @@ def about_view(request):
 
 
 def services_view(request):
-    return render(request, "main/SERVICES.html")
-
-
-def financial_info_view(request):
-    return render(request, "main/FINANCIAL-INFO.html")
+    template = ""
+    if request.path == "/services/for-individuals/":
+        template = "main/services_for_individuals.html"
+    elif request.path == "/services/for-churches-institutions/":
+        template = "main/services_for_churches_institutions.html"
+    elif request.path == "/services/for-professional-advisory/":
+        template = "main/services_for_professional_advisory.html"
+    return render(request, template)
 
 
 def ministries_view(request):
-    return render(request, "main/MINISTRIES.html")
+    template = ""
+    if request.path == "/ministries/grants/":
+        template = "main/ministries_grants.html"
+    elif request.path == "/ministries/seminary-scholarships/":
+        template = "main/ministries_seminary_scholarships.html"
+
+    elif request.path == "/ministries/for-professional-advisory/":
+        template = "main/ministries_professional_advisory.html"
+    return render(request, template)
+
+
+def financial_info_view(request):
+    template = ""
+    if request.path == "/financial-info/terms-to-know/":
+        template = "main/financial_info_terms_to_know.html"
+    elif request.path == "/financial-info/investment-philosophy/":
+        template = "main/financial_info_investment_philosophy.html"
+
+    elif request.path == "/financial-info/wespath-institutional-investments/":
+        template = "main/financial_info_wespath_institutional_investments.html"
+    return render(request, template)
 
 
 def news_events_view(request):
-    return render(request, "main/news_events.html")
+    template = ""
+    if request.path == "/news-events/foundation-news/":
+        template = "main/news_events_foundation_news.html"
+    elif request.path == "/news-events/legacies-newsletter/":
+        template = "main/news_events_legacies_newsletter.html"
+
+    elif request.path == "/news-events/market-financial-news/":
+        template = "main/financial_info_wespath_institutional_investments.html"
+
+    elif request.path == "/news-events/events/":
+        template = "main/news_events_events.html"
+
+    return render(request, template)
 
 
 def give_a_gift_view(request):
-    return render(request, "main/give_a_gift.html")
+    template = ""
+    if request.path == "/give-a-gift/donate-now/":
+        template = "main/give_a_gift_donate_now.html"
+    elif request.path == "/give-a-gift/ways-to-give/":
+        template = "main/give_a_gift_ways_to_give.html"
+
+    return render(request, template)
